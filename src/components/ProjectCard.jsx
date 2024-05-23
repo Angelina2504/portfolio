3ccb3d848eb30bc792752fed1140a271434a69d7
Angélina Droz <angieso91@gmail.com>
2024-05-23 10:11:03 +0200

    first view Css

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,15 +1,14 @@
-
 import '../styles/projetCard.css'
 
 function ProjectCard ({projectCard}) {
     return (
         <div className='projetCard-container'>
-            <h2>{projectCard.name}</h2>
-            <img src={projectCard.image} alt={`logo ${projectCard.name}`} />
-            <p>{projectCard.description}</p>
-            <p>{projectCard.techno}</p>
-            <link rel="stylesheet" href={projectCard.site} />
-            <link rel="stylesheet" href={projectCard.repo} />
+            <h2 className="h2Card">{projectCard.name}</h2>
+            <img className='imgCard' src={projectCard.image} alt={`logo ${projectCard.name}`} />
+            <p className='pCard'>{projectCard.description}</p>
+            <p className='techCard'>{projectCard.techno}</p>
+            <link className='liensSite' rel="stylesheet" href={projectCard.site} />
+            <link className='liensrepo' rel="stylesheet" href={projectCard.repo} />
         </div>
     )
 }
