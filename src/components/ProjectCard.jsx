7b7d335916b34924204914f0e6de6cd7fb510d7c
Angélina Droz <angieso91@gmail.com>
2024-06-10 17:19:07 +0200

    merge conflict

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,20 +1,11 @@
 import '../styles/projetCard.css'
 
 
-
-function ProjectCard (projectCard) {
-   
-
-<<<<<<< HEAD
-=======
-
-
 function ProjectCard ({projectCard}) {
->>>>>>> 3877c45b73c34444943045972ced065c820bb6da
+
     return (
         <div className='card-container'>
-        <div className='projetCard-container'>
-<<<<<<< HEAD
+         <div className='projetCard-container'>
             
                 <h2>{projectCard.name}</h2>
                 <img src={projectCard.image} alt={`logo ${projectCard.name}`}/>
@@ -22,16 +13,8 @@ function ProjectCard ({projectCard}) {
                 <p>{projectCard.techno}</p>
                 <a rel="stylesheet" target="_blank" href={projectCard.site} />
                 <a rel="stylesheet" target="_blank" href={projectCard.repo} />
-=======
-            <h2 className="h2Card">{projectCard.name}</h2>
-            <img className='imgCard' src={projectCard.image} alt={`logo ${projectCard.name}`} />
-            <p className='pCard'>{projectCard.description}</p>
-            <p className='techCard'>{projectCard.techno}</p>
-            <a className='liensSite' rel="stylesheet" href={projectCard.site} >Site</a>
-            <a className='liensRepo' rel="stylesheet" href={projectCard.repo} >Github</a>
+
         </div>
->>>>>>> 3877c45b73c34444943045972ced065c820bb6da
-        </div>
-                )} 
+        </div> )} 
 
 export default ProjectCard ;
