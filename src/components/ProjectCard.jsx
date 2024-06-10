8ca5a30ef501158838d591676f8932ada80d5e95
Angélina Droz <angieso91@gmail.com>
2024-06-10 22:28:21 +0200

    Test import.meta.glob of vite

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -7,22 +7,37 @@ function ProjectCard ({projectCard}) {
 
     const [imageSrc, setImageSrc] = useState('');
 
+    const images = import.meta.glob('../assets/*.png',
+    {eager : true});
+
+    
     useEffect(() => {
+       if (projectCard && projectCard.image) { 
+        const imagePath = `../assets/${projectCard.image}`;
+        if (images[imagePath]) { 
+            setImageSrc(images[imagePath].default);
+        }
+    }
+}, [projectCard,images]);
+
+
+/*     useEffect(() => {
         import(`../assets/${projectCard.image}`).then((image) => {
             setImageSrc(image.default);
         }).catch((error) => {
             console.error(`L'image ${projectCard.image} n'a pas pu être chargée : `, error);
         });
-    }, [projectCard.image]);
+    }, [projectCard.image]); */
 
     return (
         <div className='card-container'>
         <div className='projetCard-container'>
             <h2 className="h2Card">{projectCard.name}</h2>
-            <img className='imgCard' src={imageSrc} alt={`logo ${projectCard.name}`} />
+            {imageSrc ? <img className='imgCard' src={imageSrc} alt={`logo ${projectCard.name}`} /> : <p> Loading...</p> }
             <p className='pCard'>{projectCard.description}</p>
             <p className='techCard'>{projectCard.techno}</p>
-            <a className='liensSite' rel="stylesheet" href={projectCard.site} >Site</a>
+            <a className='lie
+            nsSite' rel="stylesheet" href={projectCard.site} >Site</a>
             <a className='liensRepo' rel="stylesheet" href={projectCard.repo} >Github</a>
         </div>
         </div> )} 
