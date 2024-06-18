00bb19697d825973134cc5d48ff0407dfc3c59e8
Angélina Droz <angieso91@gmail.com>
2024-06-18 23:31:11 +0200

    card design adjust

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,10 @@
 import '../styles/home.css';
 import photoProfil from "../assets/photoProfile.png"
+import html5 from "/src/assets/html5.png";
+import css3 from "/src/assets/css3.png";
+import javascript from "/src/assets/js.png";
+import nodejs from "/src/assets/nodejs.png";
+import react from "/src/assets/react.png";
 
 function HomePage() {
     return (
@@ -12,9 +17,12 @@ function HomePage() {
     <img src={photoProfil} alt="Myself's Picture" />
     
     <p>Les technos que je connais : </p>
-    <img src="" alt="" />
+    <img src={html5} alt="icon html5" />
+    <img src={css3} alt="icon css3" />
+    <img src={javascript} alt="javascript" />
+    <img src={react} alt="react" />
+    <img src={nodejs} alt="nodejs" />
    
-    <p>Les outils que je manipules: </p>
     
     </section>
 
