f9807fde5268e658a0cd9144ee85f7acf2aaf0b7
Angélina Droz <angieso91@gmail.com>
2024-06-20 11:18:11 +0200

    finish home page css

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -17,14 +17,20 @@ function HomePage() {
 
     <p className='prez'>En reconversion professionnelle pour devenir développeuse web, je suis à la recherche d'une alternance pour mettre en pratique mes compétences. Avant de me lancer dans le développement web, j'ai travaillé dans différents domaines tels que l'organisation de projets culturels, la librairie, la gestion d'un pressing et en tant qu'assistante d'éducation. Ces expériences m'ont permis de développer diverses compétences en gestion de projet, en organisation et en travail en équipe !</p>
     
-    <h2>Mes technos </h2>
-    <img src={html5} alt="icon html5" />
-    <img src={css3} alt="icon css3" />
-    <img src={javascript} alt="javascript" />
-    <img src={react} alt="react" />
-    <img src={nodejs} alt="nodejs" />
-   
-    
+    <h2 className='titleTechno'>Mes technos </h2>
+
+<div className='allTechno'>
+    <div className='iconTechno'>
+        <img className='T1' src={html5} alt="icon html5" />
+        <img className="T2" src={css3} alt="icon css3" />
+        <img className="T3" src={javascript} alt="javascript" />
+        <img className="T4" src={react} alt="react" />
+     </div>  
+     <div className='iconTechno2'>
+        <img className="T5" src={nodejs} alt="nodejs" />
+    </div>
+</div>
+
     </section>
 
     
