b92bf4b037016aa778bd259e44bd4ad114c41fdf
Angie Perso <adroz.perso@gmail.com>
2024-11-25 10:08:55 +0100

    prepare upgrade to react-router-dom v7

--- src/main.jsx
+++ src/main.jsx
@@ -18,12 +18,26 @@ const router = createBrowserRouter([
           element: <HomePage />,
         },
         {
+         /*
+         react-router-dom v7 does not support nested routes anymore, like v6 does:
+           // v6 nesting routing would look like this (with no nested routes here):
           path: "/projects",
           element: <ProjectsPage />,
+          */
+         // v7 routing with nested routes required to split the nested routes into children array:
+          path: "/projects",
+            children: [
+                { path: "", element: <ProjectsPage /> }, // For /projects base route (existing one)
+                { path: "*", element: <ProjectsPage /> } // For routes under /projects/*
+            ]
         },
-        ]
+    ]
   },
-]);
+], {
+    future: {
+        v7_relativeSplatPath: true
+    }
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
